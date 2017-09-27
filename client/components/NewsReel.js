bd65f52c378eefc0e1404ab502eca83dc35679cd
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-27 17:52:54 -0400

    add react Request package to make api requests into promises

---
+++ client/components/NewsReel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+import store, {  } from '../store';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+export default class NewsReel extends Component {
+  constructor(props){
+      super(props)
+  }
+
+  render() {
+      return (
+          <div>
+
+          </div>
+      )
+  }
+}
+
+
