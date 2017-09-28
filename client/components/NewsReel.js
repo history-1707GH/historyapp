c303e6a812dac893f5a7d141a859db1183715c81
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-28 11:49:15 -0400

    moved header to a Login component to only render on a specific route, set up for flexible development

--- client/components/NewsReel.js
+++ client/components/NewsReel.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import store from '../store';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 
-export default class NewsReel extends Component {
+class NewsReel extends Component {
   constructor(props){
       super(props)
   }
@@ -13,9 +13,6 @@ export default class NewsReel extends Component {
   render() {
       return (
           <div>
-            {
-              
-            }
           </div>
       )
   }
--- client/store/newsreel.js
+++ client/store/newsreel.js
@@ -10,17 +10,19 @@ export const setHeadlines = headlines => {type:SET_NEWS_HEADLINES, headlines};
 export default (headlines=[], action) => {
   switch (action.type){
     // I don't foresee us needing to "ADD" to the headlines so whenever we are fetching headlines i'm assuming we are resetting the Global State for the headlines of the current event/point of interest at a time 
-    case SET_NEWS_HEADLINES: return action.headlines
+    case SET_NEWS_HEADLINES: return action.headlines;
+    default: return headlines;
   }
 }
 
 // THUNKS 
 export const fetchHeadlines = query => dispatch => {
+  let fakeQuery = 'New York Stock Exchange Great Depression';
   return Request({
-    uri: 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+    uri: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
     qs: {
-      'api-key':proces.env.SECRETS.API.KEY,
-      q:`${query}`,
+      'api-key':'f91c4a6aa0254f31b9b3b807043fdff0',
+      q:`${fakeQuery}`,
       sort:'oldest',
       fl:'snippet, lead_paragraph, abstract, headline, keywords, pub_date, document_type, byline, _id, multimedia'
     },
