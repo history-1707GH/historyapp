6b137ce3ee7d96319e9487b3d2b86a457b356ab7
Erika Newland <erika.s.newland@gmail.com>
2017-10-02 17:06:14 -0400

    chore: merging w current synopsis branch

--- client/components/NextExperience.js
+++ client/components/NextExperience.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
- function NextExperience(props) {
+function NextExperience(props) {
   const textToParse = props.synopsis.content
   console.log("textToParse", textToParse)
-  const topics = nlp(textToParse).topics().data()
-  locationsArray = topics.reduce(topic=>{
+  const topics = nlp(textToParse).nouns().data()
+  console.log("topics", topics)
+  // locationsArray = topics.reduce(topic => {
+ç
 
-  }, [])
+  // }, [])
 
-    return (
-      <div>Next Up!</div>
-    )
+  return (
+    <div>Next Up!</div>
+  )
 
 }
 
@@ -22,3 +24,5 @@ const mapState = state => {
 }
 
 export default connect(mapState)(NextExperience)
+
+//get a lits of all landmarks within 1 or 0.5 miles of user and parse text looking for a referece to that like I did w the map OR compare the topics array or nouns array to that
