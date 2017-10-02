3ec6aa974797eeb483da666a1591d0a58ebfd305
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-10-02 15:56:37 -0400

    some color styling on the main page

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import store from '../store';
+import AppBar from 'material-ui/AppBar';
+import {teal900} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+
 // import { connect } from 'react-redux';
 // import Center from 'react-center';
 
@@ -16,14 +22,21 @@ export default class Login extends Component {
     document.body.className=null;
   }
   
+
   render(){
+    const styles = {
+      title: {
+        cursor: 'pointer',
+      },
+    };
     return(
-      <div>
-        <h1>Hello Testing!</h1>
-      </div>
+      <AppBar
+        title={<span style={styles.title}>meander</span>}
+        iconElementLeft={<IconButton></IconButton>}
+        iconElementRight={<FlatButton label="Login / Sign Up" />}
+        style={{backgroundColor:teal900}}
+      />
     )
   }
 }
 
-
- 
--- client/index.js
+++ client/index.js
@@ -5,8 +5,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store'
 import Main from './components/Main';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import './index.scss';
 
+// const muiTheme = getMuiTheme({
+//   palette: {
+//     textColor: teal500,
+//   },
+//   appBar: {
+//     height: 50,
+//   },
+// });
 
 const App = () => (
   <MuiThemeProvider >
