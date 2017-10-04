abd30f5d88cf68e26a3cd7bb7afae4cfc14f15c4
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-10-04 17:10:20 -0400

    add gzip and wepback minify to improve performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -5,6 +9,23 @@ module.exports = {
     filename: './public/bundle.js'
   },
   devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks 
+    new CompressionPlugin({   
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
   module: {
     rules: [
       {
