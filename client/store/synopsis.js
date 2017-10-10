90c1a08aaa1607e86dffbcae2b11fc5655cc4d53
Anna Chen <xqchen91@gmail.com>
2017-10-10 09:32:16 -0400

    fix: lag when loading synopsis no longer loads previous synopsis.

--- client/store/synopsis.js
+++ client/store/synopsis.js
@@ -1,13 +1,19 @@
 
 const GET_SYNOPSIS = 'GET_SYNOPSIS'
+const REMOVE_SYNOPSIS = 'REMOVE_SYNOPSIS'
 
 export const getSynopsis  = synopsis => {
   return {type: GET_SYNOPSIS, synopsis}
 }
 
+export const removeSynopsis  = () => {
+  return {type: REMOVE_SYNOPSIS}
+}
+
 export default function (state={}, action){
   switch(action.type) {
     case GET_SYNOPSIS: return action.synopsis
+    case REMOVE_SYNOPSIS: return {}
     default: return state
   }
 }
