4a20b51ee4c658ce417ee2a3935cc2f94f6fd830
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-10-11 13:32:58 -0400

    trying out logic for see all leaderboard

--- client/components/UserRankings.js
+++ client/components/UserRankings.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchRankings } from '../store/index';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { fetchRankings } from '../store/index'
 import FlatButton from 'material-ui/FlatButton'
 import Center from 'react-center'
 import Table, { TableBody, TableHeaderColumn, TableHeader, TableRow, TableRowColumn } from 'material-ui/Table'
+import {  teal500, white, teal900 } from 'material-ui/styles/colors'
 
 class UserRankings extends Component {
 
@@ -17,6 +18,11 @@ class UserRankings extends Component {
   }
 
   render() {
+    let leaders = this.props.rankings
+    while (leaders.length < 10) {
+      leaders = [...leaders, {username:null, points:0}]
+    }
+  
     return (
       <div className="leaderboard">
         <Center>
@@ -26,7 +32,7 @@ class UserRankings extends Component {
           <div className="leader-table">
           <Table>
             <TableHeader displaySelectAll={false} adjustForCheckbox={false} > 
-              <TableRow>
+              <TableRow style={{color:teal900}}>
                 <TableHeaderColumn style={{width: '25%', textAlign: 'center'}}><b>Rank</b></TableHeaderColumn>
                 <TableHeaderColumn style={{width: '50%', textAlign: 'justify'}}><b>User</b></TableHeaderColumn>
                 <TableHeaderColumn style={{width: '25%', textAlign: 'center'}}><b>Points</b></TableHeaderColumn>
@@ -35,7 +41,7 @@ class UserRankings extends Component {
             <TableBody displayRowCheckbox={false}>
               {this.props.rankings.map((user, index) => (
                 <TableRow key={user.id}>
-                  <TableRowColumn style={{width: '25%', textAlign: 'center'}}>{index + 1}</TableRowColumn>
+                  <TableRowColumn style={{width: '25%', textAlign: 'center', color:teal900}}>{index + 1}</TableRowColumn>
                   <TableRowColumn style={{width: '50%', textAlign: 'justify'}}>{user.username}</TableRowColumn>
                   <TableRowColumn style={{width: '25%', textAlign: 'center'}}>{user.points}</TableRowColumn>
                 </TableRow>
